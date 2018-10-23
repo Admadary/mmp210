c16e41abc87da3c4a811ce84f27360b0ef3586a0
Bryan <bryansuncar@gmail.com>
2018-10-23 13:21:52 -0400

    Finishing my memev3
    Final edition of my meme

--- meme_v3/sketch.js
+++ meme_v3/sketch.js
@@ -1,4 +1,4 @@
-/*Second Version of my Meme Project. Conditional Logic.*/
+/*Third Version of my Meme Project. Conditional Logic.*/
 
 
 function setup() {
@@ -6,28 +6,96 @@ function setup() {
 }
 
 function preload() {
-    img = loadImage("donaldtrump.jpg");
-    img2 = loadImage("reaction.png");
-    img3 = loadImage("reaction2.jpg");
+    img = loadImage("donaldvshillary.jpg");
+    img2 = loadImage("trap.jpg");
+    img3 = loadImage("card.jpg");
+    img4 = loadImage("finalform.jpg");
+    img5 = loadImage("fight.jpg");
+    img6 = loadImage("head.png");
+    img7 = loadImage("head2.png");
+    img8 = loadImage("head3.png");
 }
+
+    //Variable for Animation
+    var speed = 5;
+    var speed2 = 8
+    var x = 100;
+    var y = 100;
 
 function draw() {
     background(220);
     
-    image(img, 0, 0, width, height);
-    if (mouseX > width / 2) {
-    image(img2, 500, 0, 500, 600);
+    var sizeT = 30 // Text Size
+    var fillT = 250 // Text Color
+    var xy = 0 // X and Y position for image
+    var positionTx = 200 // X position for text
+    var positionTy = 40 // Y position for text
+    
+    var str1 = "Are you ready Burger King to lose in 2020?";
+    
+
+    
+    if (mouseX > 1) {
+        image(img, xy, xy, width, height);
+        textSize(sizeT);
+        fill(fillT);
+        stroke('black');
+        textFont("Impact");
+        text(str1, positionTx, positionTy);
+        
+    } 
+    
+    
+    
+
+    
+    var imgW2 = 600 // Width for img2
+    var imgH2 = 450 // Height for img2
+    
+    if (mouseX > 500) {
+        image(img2, xy, xy, imgW2, imgH2);
     }
-    else {
-    image(img3, 0, 0, 500, 600);
+    
+    var y3 = 250 // Y axis for img3
+    var imgW3 = 500 // Width for img3
+    
+    if ( mouseY > height / 2 ) {
+        background(fillT); // I use fillT variable because is the same color that I want.
+        image(img3, y3, xy, imgW3, height);
     }
     
-    var str1 = "My reaction when Ronald McDonald won the election";
+    var str2 = "Are you ready for my final form Hillary?";
     
-    textSize(30);
-    fill(250);
-    stroke('black');
-    textFont("Impact");
-    text(str1, 200, 40);
+    if ( mouseY > height / 2 && mouseX > 500) {
+        image(img4, xy, xy, width, height);
+        textSize(sizeT);
+        fill(fillT);
+        stroke('black');
+        textFont("Impact");
+        text(str2, positionTx, positionTy);
+    }
     
+
+    
+    image(img6, x, 300, 150, 206);
+    x += speed;
+    	if (x > width) {
+		x = 0;
+	}
+    
+    image(img7, 100, y, 90, 128.75);
+    y += speed2;
+        if (y > height) {
+            y = 0;
+        }
+    
+    var r = frameCount / 500 * PI;
+    shearX(r);
+
+    image(img5, 500, 150);
+    
+  
+  
+    
+
 }
